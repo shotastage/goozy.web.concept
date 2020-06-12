0c04f2e84fb5c4367a7083220983207b53eddb1d
Shota Shimazu <hornet.live.mf@gmail.com>
2020-06-12 09:27:08 +0900

    add new app

--- goozs/views.py
+++ goozs/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, FormView, ListView
+from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from goozs.models import Gooz
 
 # Create your views here.
 
 
-class GoozsIndexView(ListView):
+class GoozsIndexView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
     template_name = "index.html"
     paginate_by = 20
 
     model = Gooz
 
 
-class GoozsCreateView(CreateView):
+class GoozsCreateView(LoginRequiredMixin, CreateView):
     template_name = "register.html"
-
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
     model = Gooz
     fields = ['name', 'serial', 'photo']
     success_url = "/"
